2f1426c131f4afab3f910095f19f8e3a38fa7204
heisje <heeje1996@gmail.com>
2024-08-02 10:40:25 +0900

    Merge: develop

--- src/fetch/fetch.js
+++ src/fetch/fetch.js
@@ -1,44 +1,32 @@
+export const DOMAIN_URL = 'http://localhost:8080';
+
+// fetch, tryCatch로 싸줘야함
 export async function fetchData(url, options = {}) {
+    const response = await fetch(url, options);
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return await response.json();
+}
+
+export async function defaultErrorBoundary(fetchFunction) {
     try {
-        const response = await fetch(url, options);
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return await response.json();
+        return await fetchFunction();
     } catch (error) {
         console.error('Failed to fetch data:', error);
-        alert('요청에 실패했습니다.')
+        const errorMessage = await response.text();
+        alert(`요청에 실패했습니다.: ${errorMessage}`);
         return null;
     }
 }
 
-export async function insertData(item) {
-    try {
-        console.log("Inserting data:", item);
-
-        // Fetch 요청을 사용하여 데이터를 서버에 전송합니다.
-        const response = await fetch('http://localhost:8080/income', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(item)
-        });
-
-        // 응답이 성공적이면 알림을 표시합니다.
-        if (response.ok) {
-            alert("등록에 성공하였습니다");
-            return await response.json(); // 응답 데이터를 반환합니다.
-        } else {
-            // 서버가 에러를 반환할 경우 에러 메시지를 표시합니다.
-            const errorMessage = await response.text();
-            alert(`등록에 실패하였습니다: ${errorMessage}`);
-            return null;
-        }
-    } catch (error) {
-        // 네트워크 오류 등의 예외 상황을 처리합니다.
-        console.error('Failed to fetch data:', error);
-        alert('요청에 실패했습니다.');
-        return null;
-    }
+// 쿼리를 
+export function objectToQueryParams(obj) {
+    return Object.entries(obj)
+        .map(([key, value]) => {
+            return `${encodeURIComponent(key)}=${encodeURIComponent(value)}`;
+        })
+        .join('&');
 }
+
+
--- src/monthlyView.js
+++ src/monthlyView.js
@@ -3,13 +3,22 @@ import { createList } from "./components/acountItem.js"
 
 document.getElementById('monthPicker').addEventListener('change', onChangeMonth);
 
-async function onChangeMonth(event) {
-    const selectedMonth = event.target.value; // yyyy-MM
+const form = document.getElementById('filter-form');
+
+form.addEventListener('change', (event) => {
+    const monthElement = document.getElementById('monthPicker');
+    const selectElement = document.getElementById('options');
+
+    onChangeMonth(monthElement.value, selectElement.value);
+});
+
+
+async function onChangeMonth(date, ie) {
     const oldTbody = document.getElementById('results-tbody');
     const newTbody = oldTbody.cloneNode(false);
     oldTbody.replaceWith(newTbody);
 
-    const response = await monthlyViewRequest(selectedMonth);
+    const response = await monthlyViewRequest(date, ie);
     createList(response);
 }
 
